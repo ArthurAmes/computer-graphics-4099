b18d99ff0fce6be2f100818714bbd1c0d1dd30f7
Arthur Ames <aames@wpi.edu>
2023-10-19 15:53:19 -0400

    forgot to save

--- assignment_3_code/main.js
+++ assignment_3_code/main.js
@@ -1,5 +1,5 @@
-import { default as seagulls } from '../../seagulls.js'
-import { default as Mouse    } from '../../mouse.js'
+import { default as seagulls } from './seagulls.js'
+import { default as Mouse    } from './mouse.js'
 import { Pane } from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.1/dist/tweakpane.min.js';
 
 const w = window.innerWidth;
